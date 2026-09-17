8808a678fffa1543fa4b61ce2366e3a05c8b8e21
Lucille Cortot <cortot.lucille@gmail.com>
2026-09-17 09:48:29 +0200

    Phase 8 : ORM (tri des enregistrements).

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -14,11 +14,18 @@ class VoyagesController extends AbstractController {
 
     #[Route('/voyages', name: 'voyages')]
     public function index() : Response {
-        $visites = $this->repository->findAll();
-        return $this -> render("pages/voyages.html.twig", 
-                [
+        $visites = $this->repository->findAllOrderBy('datecreation', 'DESC');
+        return $this -> render("pages/voyages.html.twig", [
                     'visites' => $visites
                 ]);
+    }
+    
+    #[Route('/voyage/tri/{champ}/{ordre}', name: 'voyages.sort')]
+    public function sort($champ, $ordre): Response{
+        $visites = $this->repository->findAllOrderBy($champ, $ordre);
+        return $this->render("pages/voyages.html.twig", [
+            'visites' => $visites
+        ]);
     }
     
    /**
