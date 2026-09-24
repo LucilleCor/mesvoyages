0de11dd01850caa878d78a66a8ae328e536f5b59
Lucille Cortot <cortot.lucille@gmail.com>
2026-09-24 16:03:46 +0200

    Phase 23 : tests (compléments)

--- tests/VisiteTest.php
+++ tests/VisiteTest.php
@@ -1,12 +1,8 @@
 <?php
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
- */
-
 namespace App\Tests;
 
+use App\Entity\Environnement;
 use App\Entity\Visite;
 use DateTime;
 use PHPUnit\Framework\TestCase;
@@ -22,5 +18,16 @@ class VisiteTest extends TestCase {
         $visite = new Visite();
         $visite->setDatecreation (new DateTime("2024-04-24"));
         $this->assertEquals("24/04/2024", $visite->getDatecreationString());
+    }
+    
+    public function testAddEnvironnement(){
+        $environnement = new Environnement();
+        $environnement->setNom("plage");
+        $visite = new Visite();
+        $visite->addEnvironnement($environnement);
+        $nbEnvironnementAvant = $visite->getEnvironnements()->count();
+        $visite->addEnvironnement($environnement);
+        $nbEnvironnementApres = $visite->getEnvironnements()->count();
+        $this->assertEquals($nbEnvironnementAvant, $nbEnvironnementApres, "ajout même environnement devrait échouer");
     }
 }
